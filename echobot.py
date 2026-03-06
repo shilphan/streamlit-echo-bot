4ce23394ea091de7a4dc484c74617811de810486
shilphan <sgphanse@gmail.com>
2026-03-07 00:33:58 +0530

    Change greeting and include random bar chart
    Updated greeting message and added a bar chart.

--- echobot.py
+++ echobot.py
@@ -1,6 +1,7 @@
 
 import streamlit as st
+import numpy as np
 
 with st.chat_message("assistant"):
-    st.write("Hello, How is it going? ")
- 
+    st.write("Hello human")
+    st.bar_chart(np.random.randn(30, 3))
